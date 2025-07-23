57ea2ab7bc1bf1001b2c5cff6fac8a7f60a95d60
SantiagoPerezAgustin <santiagus153@gmail.com>
2025-07-23 00:18:09 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './page/Home';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'; // Asegúrate de tener este archivo para estilos globales
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App
---
+++ src/page/Home.jsx
@@ -0,0 +1,12 @@
+import MyNavbar from "../components/Navbar/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <MyNavbar />
+      <h1>¡Bienvenido a Brisa Calzado!</h1>
+    </div>
+  );
+}
+
+export default Home;
