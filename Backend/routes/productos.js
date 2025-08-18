a5378a3a3300d79b00af3f2b2ded470d8c5cc83f
SantiagoPerezAgustin <santiagus153@gmail.com>
2025-08-18 19:14:34 -0300

    Otro arreglo

--- Backend/routes/productos.js
+++ Backend/routes/productos.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productosController = require("../controllers/productosControllers");
 
 router.get("/", productosController.getProductos);
-router.get("/:id", productosController.getProductoById);
+router.get("/:id", productosController.getProductoById); // Esta es la ruta que probablemente faltaba
 router.post("/", productosController.createProducto);
 router.put("/:id", productosController.updateProducto);
 router.delete("/:id", productosController.deleteProducto);
